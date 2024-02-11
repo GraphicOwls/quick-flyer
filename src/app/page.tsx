f796ded1d14e8b00e15cf2f49af68205df55bed6
Noah Raskin <noahgraskin@gmail.com>
2024-02-11 04:20:45 -0500

    Adds download image functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,39 @@
 /** @format */
+'use client'
 
 import { FlyerControls } from '@/components/flyer-controls'
 import FlyerOne from '@/components/flyer-designs/flyer-01'
 import { ModeToggle } from '@/components/mode-toggle'
 import { Button } from '@/components/ui/button'
-import { Download, HardDriveDownload, Zap } from 'lucide-react'
+import { HardDriveDownload, Zap } from 'lucide-react'
+import html2canvas from 'html2canvas'
+import { backgroundClip } from 'html2canvas/dist/types/css/property-descriptors/background-clip'
 
 export default function Home() {
+	function downloadImage() {
+		const canvas = document.querySelector('.flyer-element')
+		// @ts-ignore
+		html2canvas(canvas, { backgroundColor: 'null' }).then((canvas) => {
+			const link = document.createElement('a')
+			link.download = 'flyer.png'
+			link.href = canvas.toDataURL('image/png')
+			link.click()
+		})
+	}
+
 	return (
 		<main className='h-svh'>
 			<div className='flex items-stretch justify-stretch h-full'>
 				{/* CONTAINER */}
 				<div className='flex flex-col items-center justify-center flex-grow relative p-6 canvas-container'>
 					{/* CANVAS */}
-					<div className='w-full max-w-[620px] aspect-3/4 bg-tertiary border border-border rounded-xl overflow-hidden relative z-20 p-6'>
+					<div className='flyer-element w-full max-w-[620px] aspect-3/4 bg-tertiary border border-border rounded-3xl overflow-hidden relative z-20 p-6'>
 						<FlyerOne />
 					</div>
 					<Button
 						className='w-full mt-3 max-w-[620px] flex items-center justify-center'
 						variant={'outline'}
+						onClick={downloadImage}
 					>
 						Download Image
 						<HardDriveDownload className='ml-3 h-4 w-4' />
